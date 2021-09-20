cfd1618cb712699040450b106f368ca8fd2fbd67
IrvinA <arevalosirvin@gmail.com>
2021-09-20 13:56:08 -0700

    Styled app, home, and organizer.js

--- potluck/src/Components/App.js
+++ potluck/src/Components/App.js
@@ -3,18 +3,62 @@ import { Route, Switch, Link } from 'react-router-dom';
 import Home from './home';
 import Organizer from './organizer';
 import Guest from './guest';
+import styled from 'styled-components';
+
+const StyledNav = styled.nav`
+  background-image: url('https://images.unsplash.com/photo-1498676077434-7540603d2dda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80');
+  background-size: cover;
+  background-repeat: no-repeat;
+  margin: 0 5%;
+  display: flex;
+  justify-content: space-between;
+  padding: 5%;
+
+  h1 {
+    border: 2px solid orange;
+    background-image: linear-gradient(to left, green, crimson);
+    opacity: 80%;
+    padding: 3% 2%;
+    border-radius: 30px;
+    text-shadow: 1px 1px orange;
+    color: crimson;
+    font-weight: 800;
+    letter-spacing: 1.2px;
+  }
+
+  span {
+    color: green;
+  }
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 30%;
+  }
+
+  a {
+    text-decoration: none;
+    color: black;
+    font-size: 1.5rem;
+    border: 1px solid orange;
+    background-color: white;
+    padding: 2% 4%;
+    border-radius: 20px;
+  }
+`
 
 const App = () => {
   return (
     <>
-      <nav>
-        <h1>Potluck Planner</h1>
+      <StyledNav>
+        <h1><span>Potluck</span> Planner</h1>
         <div>
           <Link to='/'>Home</Link>
-          <Link to='/organizer'>Potluck Organizer</Link>
-          <Link to='/guest'>Potluck Guest</Link>
+          <Link to='/organizer'>Organizer</Link>
+          <Link to='/guest'>Guest</Link>
         </div>
-      </nav>
+      </StyledNav>
       {
         <Switch>
           <Route path='/organizer'>
--- potluck/src/Components/home.js
+++ potluck/src/Components/home.js
@@ -1,5 +1,87 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+    margin: 0 5%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-image: url('https://images.unsplash.com/photo-1511795409834-ef04bbd61622?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80');
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 800px;
+
+    div {
+        border: 2px solid black;
+        padding: 10% 5%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    .left{
+        margin-left: 15%;
+        background-color: crimson;
+        opacity: 80%;
+        border-radius: 50px;
+    }
+
+    .left h2 {
+        margin-bottom: 30%;
+        border: 2px solid black;
+        color: green;
+        border-radius: 20px;
+        padding: 4% 1%;
+        background-color: white;
+        text-align: center;
+    }
+
+    .left button{
+        padding: 5% 0;
+        font-weight: 800;
+        color: green;
+        border-radius: 20px;
+        background-color: white;
+        &:hover {
+            transition: all 1s;
+            transform: scale(1.2);
+            background-color: black;
+            color: crimson;
+        }
+    }
+
+    .right {
+        margin-right: 15%;
+        background-color: green;
+        opacity: 80%;
+        border-radius: 50px;
+    }
+
+    .right h2 {
+        margin-bottom: 30%;
+        border: 2px solid black;
+        color: crimson;
+        border-radius: 20px;
+        padding: 4% 1%;
+        text-align: center;
+        background-color: white;
+    }
+
+    .right button{
+        padding: 5% 0;
+        font-weight: 800;
+        color: crimson;
+        border-radius: 20px;
+        background-color: white;
+        &:hover {
+            transition: all 1s;
+            transform: scale(1.2);
+            background-color: black;
+            color: green;
+        }
+    }
+`
 
 export default function Home() {
     const history = useHistory();
@@ -11,15 +93,15 @@ export default function Home() {
     }
 
     return (
-        <div>
-            <div>
+        <StyledDiv>
+            <div className='left'>
                 <h2>Organizing a potluck?</h2>
                 <button onClick={routeToOrganizer}>Sign up here</button>
             </div>
-            <div>
+            <div className='right'>
                 <h2>Invited to a potluck?</h2>
                 <button onClick={routeToGuest}>Guest login</button>
             </div>
-        </div>
+        </StyledDiv>
     )
 }
