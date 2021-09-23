12fe261c0a8c285678a18a3193e8c9a9c46a4da5
Max Tilden <maxtilden@gmail.com>
2021-09-22 19:16:33 -0700

    made a correction

--- potluck/src/Components/App.js
+++ potluck/src/Components/App.js
@@ -6,14 +6,7 @@ import Guest from './guest';
 import styled from 'styled-components';
 import { AccountBox } from "./accountBox";
 
-const AppContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
+
 
 const StyledNav = styled.nav`
   background-image: url('https://images.unsplash.com/photo-1498676077434-7540603d2dda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80');
@@ -69,7 +62,7 @@ const App = () => {
           <Link to='/guest'>Guest</Link>
         </div>
       </StyledNav>
-      {<AppContainer>
+      
         <Switch>
         <Route path="/signin" component={AccountBox} />
           <Route path="/signup" component={AccountBox} />
@@ -86,8 +79,8 @@ const App = () => {
             <Home />
           </Route>
         </Switch>
-        </AppContainer>
-      }
+        
+      
     </BrowserRouter>
   );
 }
